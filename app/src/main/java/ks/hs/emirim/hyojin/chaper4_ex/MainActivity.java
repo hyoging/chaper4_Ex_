bf795e950e1b3a28cc60b3e94c3e4f0242a5f79f
User <w2030@e-mirim.hs.kr>
2021-03-30 21:14:20 +0900

    xml button속성 연습

---
+++ app/src/main/java/ks/hs/emirim/hyojin/chaper4_ex/MainActivity.java
@@ -0,0 +1,14 @@
+package ks.hs.emirim.hyojin.chaper4_ex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
